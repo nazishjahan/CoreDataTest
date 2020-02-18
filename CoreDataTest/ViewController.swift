fa309a409f0225fe5a7c0ac49fa38a482ee2aac7
NAZISH ZOHAIB <nazishzohaib@nazishs-macbook-pro.local>
2020-02-18 06:18:44 +0500

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by NAZISH ZOHAIB on 18/02/2020.
+//  Copyright © 2020 NAZISH ZOHAIB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
